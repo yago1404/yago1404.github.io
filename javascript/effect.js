c7fa61af42f32b5f90a40e9279d395bb3e94034a
yago1404 <ytf@ic.ufal.br>
2020-04-17 22:33:44 -0300

    Beta

---
+++ javascript/effect.js
@@ -0,0 +1,32 @@
+function transition_content(onclicked_button, div_to_change){
+    clear_elements();
+    document.getElementById(onclicked_button).style.padding = "10px 30px";
+    document.getElementById(onclicked_button).style.border = "3px solid #A40610";
+    document.getElementById(div_to_change).style.zIndex = "10";
+}
+
+function clear_elements(){
+    var elements = ['web-developer-button', 'mobile-developer-button', 'design-button', 'twig-button'];
+    for (let element of elements){
+        document.getElementById(element).style.border = "0px";
+        document.getElementById(element).style.padding = "0px";
+    }
+    var div_elements = ['design-div', 'web-developer-div', 'mobile-developer-div', 'twig-div'];
+    for (let element of div_elements){
+        document.getElementById(element).style.zIndex = "0";
+    }
+}
+function load_page(){
+    document.getElementById('design-button').style.padding = "10px 30px";
+    document.getElementById('design-button').style.border = "3px solid #A40610";
+    document.getElementById('design-div').style.zIndex = "10";
+
+}
+
+function scroll_page(pos){
+    window.scroll({
+        top: pos, 
+        left: 0, 
+        behavior: 'smooth'
+    });
+}
